9c37799e99249d607e5f20ba7830bc71a56fe6f2
Doğuş Can Çallı <dqampire@gmail.com>
2022-04-10 19:38:38 +0300

    Update skills.js

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,6 +1,6 @@
 const skills = [
-  ['HTML', 'CSS', 'Javascript', 'React', 'Gatsby', 'Next'],
-  ['NodeJS', 'Express', 'MongoDB', 'Firebase', 'Contentful', 'Adobe XD'],
+  ['HTML', 'CSS', 'Javascript', 'React', 'Next', 'Gatsby', 'Styled Components', 'Tailwind', 'GSAP', 'Framer Motion'],
+  ['NodeJS', 'Express', 'MongoDB', 'GraphQL', 'Apollo', 'Firebase', 'Contentful', 'Adobe XD'],
 ]
 
 export default skills
